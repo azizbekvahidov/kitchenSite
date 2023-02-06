6e5ba527e9baf8f98e45e3df452813e519c6dd40
farukh shomurodov <you@example.com>
2023-02-06 22:09:46 +0500

    changes in view

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,67 +1,35 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Kitchen | Dashboard</title>
-    <link rel="stylesheet"
-          href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-    <link rel="stylesheet" href="{{ asset("plugins/fontawesome-free/css/all.min.css") }}">
-    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-    <link rel="stylesheet" href="{{asset("dist/css/adminlte.min.css")}}">
-    <link rel="stylesheet" href="{{asset("plugins/overlayScrollbars/css/OverlayScrollbars.min.css")}}">
-    <link rel="stylesheet" href="{{asset("plugins/summernote/summernote-bs4.min.css")}}">
-</head>
-<body class="hold-transition sidebar-mini layout-fixed">
-<div class="wrapper">
+@extends('layouts.app')
 
-    @include('components.navbar')
-    @include('components.sidebar')
+@section('main-content')
+    <div class="wrapper">
 
-    <div class="content-wrapper">
-        <!-- Content Header (Page header) -->
-        <div class="content-header">
-            <div class="container-fluid">
-                <div class="row mb-2">
-                    <div class="col-sm-6">
-                        <h1 class="m-0">@yield('content-header')</h1>
-                    </div><!-- /.col -->
-                    <div class="col-sm-6">
-                        @include('components.breadcrumbs')
-                    </div><!-- /.col -->
-                </div><!-- /.row -->
-            </div><!-- /.container-fluid -->
+        @include('components.navbar')
+        @include('components.sidebar')
+
+        <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+            <div class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
+                            <h1 class="m-0">@yield('content-header')</h1>
+                        </div><!-- /.col -->
+                        <div class="col-sm-6">
+                            @include('components.breadcrumbs')
+                        </div><!-- /.col -->
+                    </div><!-- /.row -->
+                </div><!-- /.container-fluid -->
+            </div>
+            <!-- /.content-header -->
+
+            <!-- Main content -->
+            <section class="content">
+                @yield('content')
+            </section>
+            <!-- /.content -->
         </div>
-        <!-- /.content-header -->
+        <!-- /.content-wrapper -->
+        @include('components.footer')
 
-        <!-- Main content -->
-        <section class="content">
-            @yield('content')
-        </section>
-        <!-- /.content -->
     </div>
-    <!-- /.content-wrapper -->
-    @include('components.footer')
-
-</div>
-<!-- ./wrapper -->
-
-<!-- jQuery -->
-<script src="{{asset("plugins/jquery/jquery.min.js")}}"></script>
-<!-- jQuery UI 1.11.4 -->
-<script src="{{asset("plugins/jquery-ui/jquery-ui.min.js")}}"></script>
-<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-<script>
-    $.widget.bridge('uibutton', $.ui.button)
-</script>
-<!-- Bootstrap 4 -->
-<script src="{{asset("plugins/bootstrap/js/bootstrap.bundle.min.js")}}"></script>
-
-<!-- Summernote -->
-<script src="{{asset("plugins/summernote/summernote-bs4.min.js")}}"></script>
-<!-- overlayScrollbars -->
-<script src="{{asset("plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js")}}"></script>
-<!-- AdminLTE App -->
-<script src="{{asset("dist/js/adminlte.js")}}"></script>
-</body>
-</html>
+@endsection
