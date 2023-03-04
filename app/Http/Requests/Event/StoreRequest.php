ebd7f013c6318c4697bc7c0b4d9b416dfc909a64
Umid1982 <umidurinov14@gmail.com>
2023-03-04 14:20:35 +0500

    create stock

---
+++ app/Http/Requests/Event/StoreRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests\Event;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'title_uz' => 'required|string|max:255',
+            'title_ru' => 'required|string|max:255',
+            'title_en' => 'required|string|max:255',
+            'description_uz' => 'required|string|max:255',
+            'description_ru' => 'required|string|max:255',
+            'description_en' => 'required|string|max:255',
+            'image'=>'required|mimes:jpeg,jpg,png|max:51200',
+        ];
+    }
+}
