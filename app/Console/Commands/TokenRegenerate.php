e53407e24c5c382af75efb36440dd23c00ce08fb
Umid1982 <umidurinov14@gmail.com>
2023-03-10 17:38:37 +0500

    delete end create token in artisan  console

---
+++ app/Console/Commands/TokenRegenerate.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+
+class TokenRegenerate extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'token:regenerate';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'deleting and creating an authorized user';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle(): int
+    {
+        /** @var User $user */
+        $user = User::query()->where('login', '=', 'admin')->first();
+        $user->tokens()->delete();
+        $token = $user->createToken('new token ')->plainTextToken;
+        $this->components->info('Ваш авторизованный пользователь ' .$token);
+        return Command::SUCCESS;
+    }
+}
